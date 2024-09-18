7d89ceb8d094cf52827534ec37d50435b260be98
FiX <myiphone128@mail.ru>
2024-09-18 12:46:05 +0300

    logreg

---
+++ script.js
@@ -0,0 +1,20 @@
+const signinBtn = document.getElementById("autoriz-btn");
+const overlay = document.getElementById("overlay");
+const authPopUp = document.getElementById("autrztn-popup");
+const closePopUpBtn = document.getElementById("close-popup-btn");
+const authPopupRegBtn = document.getElementById("popup-reg-btn");
+const authPopupLoginBtn = document.getElementById("popup-login-btn");
+
+const pupupToggle = () => {
+  overlay.classList.toggle("active");
+  authPopUp.classList.toggle("active");
+};
+
+const popupAuthContentToggle = () => {
+  authPopUp.classList.toggle("registration");
+};
+
+signinBtn.addEventListener("click", pupupToggle);
+closePopUpBtn.addEventListener("click", pupupToggle);
+authPopupRegBtn.addEventListener("click", popupAuthContentToggle);
+authPopupLoginBtn.addEventListener("click", popupAuthContentToggle);
